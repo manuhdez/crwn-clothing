c6a05bde7765f830fb2e374a72fdb1724e12dcaf
Manu Hdez <manu.hdez.dev@gmail.com>
2020-02-15 09:13:57 +0000

    installed and configured apollo-graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,35 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
 
 import './index.css';
 import App from './App';
 
 import store from './redux/store';
 
+const link = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
+
 ReactDOM.render(
-  <Provider store={store.store}>
-    <BrowserRouter>
-      <PersistGate persistor={store.persistore}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store.store}>
+      <BrowserRouter>
+        <PersistGate persistor={store.persistore}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
